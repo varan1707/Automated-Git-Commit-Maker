230b03990c5e5c48422e392ac6eac50f58d37cbf
Varan Mehta <varan@Varans-MacBook-Air.local>
2024-02-16 16:23:07 -0500

    Automating the commit process

---
+++ app.py
@@ -0,0 +1,20 @@
+from contextlib import nullcontext
+import os
+import subprocess
+import google.generativeai as genai
+
+GOOGLE_API_KEY = os.getenv('GOOGLE-API_KEY')
+
+genai.configure(api_key = GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+def generate_commit_message():
+    git_changes =  subprocess.run(["git", "diff", "--cached"], capture_output = True, text = True)
+    return model.generate_content('''You are an expert at cerating a github commit message for a set of changes. Here is a diff of changes we need to a commit message for: ''' + git_changes)
+
+#Some data has been taken from documentation that is offered by Google. Here is the link for the same as well "https://ai.google.dev/tutorials/rest_quickstart"
+
+if __name__ == '__main__':
+    print("Your commit message is " +  generate_commit_message().text) 
+    
