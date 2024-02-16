937c0400f5035945ceed73e5a82075b150d15634
Varan Mehta <85610764+varan1707@users.noreply.github.com>
2024-02-16 16:46:00 -0500

    Changed git_commit to be a string variable to avoid errors

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ model = genai.GenerativeModel('gemini-pro')
 
 def generate_commit_message():
     git_changes =  subprocess.run(["git", "diff", "--cached"], capture_output = True, text = True)
-    return model.generate_content('''You are an expert at cerating a github commit message for a set of changes. Here is a diff of changes we need to a commit message for: ''' + git_changes)
+    return model.generate_content('''You are an expert at creating a GitHub commit message for a set of changes. 
+    Here is a diff of changes we need to a commit message for: ''' + str(git_changes))
 
 #Some data has been taken from documentation that is offered by Google. Here is the link for the same as well "https://ai.google.dev/tutorials/rest_quickstart"
 
